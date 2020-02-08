3d72339d860d92fb811c2ea6183e5c80a436f7a2
strongdoctor <strongdoctor@live.com>
2020-02-08 15:31:12 +0200

    Initial commit, including Swagger

---
+++ Controllers/StronkGameV1Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace stronkgame.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    [Route("api/v1")]
+    public class StronkGameV1Controller : ControllerBase
+    {
+        private readonly ILogger<StronkGameV1Controller> _logger;
+
+        public StronkGameV1Controller(ILogger<StronkGameV1Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Content("{\"test\":\"Test\"}", "application/json");
+        }
+    }
+}
